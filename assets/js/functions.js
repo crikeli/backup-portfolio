cd538ecf55b69a5f2cd1feabc906806fd3010f92
Kelin Christi <christik.dev@gmail.com>
2016-11-24 22:28:28 -0800

    Fixed resizing issue on the download resume link 2

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,7 +5,7 @@ $(function() {
 	clientStuff();
 
 	$("header h1").fitText(1, { minFontSize: '20px', maxFontSize: '72px' });
-	$(".biglink").fitText(1, {minFontSize: '35px', maxFontSize: '65px'})
+	$(".biglink").fitText(1, {minFontSize: '35px', maxFontSize: '45px'})
 
 	$('textarea').autosize();
 });
