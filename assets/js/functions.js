88f61f402d2eb2e6eb95e1cee78fd56c366e22e1
crikeli <christik.dev@gmail.com>
2016-12-11 23:59:36 -0800

    Some cleanup of code

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -15,7 +15,6 @@ function smoothScroll (duration) {
 	$('a[href^="#"]').on('click', function(event) {
 
 	    var target = $( $(this).attr('href') );
-
 	    if( target.length ) {
 	        event.preventDefault();
 	        $('html, body').animate({
